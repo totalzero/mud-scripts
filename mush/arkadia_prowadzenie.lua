7fc2776fa9d2101e8a9d95edc2053c8ca0f5036b
TotalZero <radek.solekr@icloud.com>
2023-10-16 17:50:35 +0200

    ent

--- mush/arkadia_prowadzenie.lua
+++ mush/arkadia_prowadzenie.lua
@@ -1,7 +1,25 @@
 cel = ""
+rozkazy = false
 
 function ustawCel(n, l, w)
 cel = table.concat(w)
 Note("ustawiono cel:"..cel)
 end
 
+function odpalCel()
+    Note("wskaz "..cel.." jako cel ataku")
+    Note("zabij cel ataku")
+    if rozkazy == true then
+        Note("rozkaz druzynie zabij cel ataku")
+    end
+end
+
+function rozkazyOn()
+rozkazy = true
+Note("rozkazy gotowe")
+end
+
+function rozkazyOff()
+    rozkazy = false
+Note("wylaczam rozkazy")
+end
